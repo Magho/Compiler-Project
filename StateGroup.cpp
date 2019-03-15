01be7f7ce3eea48d3b47eee5da00ae2a91d26d3a
abdelrahman882 <abdelrahmanahmed882@gmail.com>
2019-03-15 21:46:54 +0200

    adding wrapper class to Node class

---
+++ StateGroup.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include "Transition.h"
+using namespace std;
+
+int main(){
+    return 0;
+}
+
+
+class StateGroup{
+public :
+    vector<Node>* states;
+    int number =-1;
+    StateGroup(int Num){
+        number = Num;
+    }
+
+    bool addState(Node toAdd){
+        for (Node s : *states){
+            if(s.Number== toAdd.Number){
+                return false;
+            }
+        }
+        states->push_back(toAdd);
+        return true;
+        }
+    bool operator ==(StateGroup s2){
+        int num = s2.states->size();
+        for(Node sElem : *states){
+            int prevNum =num;
+            for(Node s2Elem : *s2.states){
+                if(sElem.Number==s2Elem.Number){
+                    num--;
+                    break;
+                }
+            }
+            if(prevNum==num) return false;
+        }
+        if(num==0){
+            return true;
+        }else if(num > 0){
+            return false;
+        }else{
+            return false;
+            //Error a potential repeated element in group
+        }
+    }
+
+};
