d474c584bb23bb81344ca8d149aa7c344b4f0aa3
abdelrahman882 <abdelrahmanahmed882@gmail.com>
2019-03-20 05:24:00 +0200

    nfa to dfa tested

--- NFAtoDFA.cpp
+++ NFAtoDFA.cpp
@@ -14,7 +14,7 @@ private :
 
     bool isIn(vector<Node*> * nodes,Node *n ){
         for(Node *i : *nodes){
-            if(i->Number == n->Number)return true;
+            if(i->nodeNumber == n->nodeNumber)return true;
         }
         return false;
     }
@@ -22,52 +22,54 @@ private :
 
     /*find closure takes a state and finds the closure of that state and overloaded with set of states as parameters*/
     vector<Node *>* findClosure(vector<Node *>* states){
-        cout<<"find closure called with states :"<<endl;
-        for(Node *n : *states)cout<<n->Number<<" ";
-        cout<<endl;
+
         vector<Node*> * closure = new vector<Node*>();
         vector<Node *> * clStk = new vector<Node*>();
         for(Node *n : *states){
             clStk->push_back(n);
+            closure->push_back(n);
         }
         while(!clStk->empty()){
-           for(Transition t : clStk->back()->transitions){
-            if(t.transitionSymbol == EPSILON){
-                if(!isIn(closure,t.toNode)){
-                    closure->push_back(t.toNode);
-                    clStk->push_back(t.toNode);
+            vector<Transition *> temp = clStk->back()->transitions;
+            clStk->pop_back();
+           for(Transition *t : temp){
+            if(t->transitionSymbol == EPSILON){
+                if(!isIn(closure,t->toNode)){
+                    closure->push_back(t->toNode);
+                    clStk->push_back(t->toNode);
                 }
                }
            }
         }
-        cout<<"find closure resulted in states :"<<endl;
-        for(Node* n : *closure)cout<<n->Number<<" ";
-        cout<<endl<<endl;
+        if(!closure->empty()) {
+            cout << "find closure resulted in states :" << endl;
+            for (Node *n : *closure)cout << n->nodeNumber << " ";
+            cout << endl;
+        }
     return  closure;
     }
 
 
     vector<Node*>* findToStates(vector<Node*>* states,char symbol){
-        cout<<"find to called with i/p :"<<symbol<< " and curr states :"<<endl;
-        for(Node *n : *states)cout<<n->Number<<" ";
-        cout<<endl<<endl;
+
 
 
         vector<Node*> * sts = new vector<Node*>();
         for(Node *n : *states){
-           for(Transition t : n->transitions){
-            if(t.transitionSymbol == symbol){
-                if(!isIn(sts,t.toNode)){
-                    sts->push_back(t.toNode);
+           for(Transition *t : n->transitions){
+            if(t->transitionSymbol == symbol){
+                if(!isIn(sts,t->toNode)){
+                    sts->push_back(t->toNode);
                 }
                }
            }
         }
 
-
-        cout<<"find to resulted in states :"<<endl;
-        for(Node *n : *sts)cout<<n->Number<<" ";
-        cout<<endl<<endl;
+        if(!sts->empty()) {
+            cout << "find to under i/p"<<(int)symbol<<" resulted in states :" << endl;
+            for (Node *n : *sts)cout << n->nodeNumber << " ";
+            cout << endl;
+        }
         return sts;
     }
 
@@ -83,7 +85,7 @@ public :
         cout<<"converting from nfa to dfa ..."<<endl;
         /*start node given as input to that module*/
         Node * start = NFAStart;
-        cout<<"start node is "<<start->Number<<endl;
+        cout<<"start node is "<<start->nodeNumber<<endl;
 
         /*put the start state closure in the filling stack */
          vector<StateGroup*> fillingStk;
@@ -97,33 +99,38 @@ public :
         cout<<"got the start epsilon closure and set to group 0"<<endl;
 
         /*constructing fai*/
-        fai = new Node(0,false,false,"");
-        for(char i=0;i<256;i++){
-            Transition * t = new Transition();
-            t->toNode = fai;
-            t->transitionSymbol = i;
-            fai->transitions.push_back(*t);
-        }
+        fai = new Node(0,false,false,INT_MAX,"");
         StateGroup * faiG = new StateGroup(1);
         faiG->addState(fai);
-        cout<<"constructed fai and set to group 1 (not pushed to filling stack)"<<endl;
+        for(int i=0;i<128;i++){
+            Transition * t = new Transition(faiG->getGroupNode(),(char)i);
+            faiG->getGroupNode()->transitions.push_back(t);
+        }
+
+            cout<<"constructed fai and set to group 1 (not pushed to filling stack)"<<endl;
+
 
          /*looping until the stack is empty*/
         int ctr=1;
         while(!fillingStk.empty()){
-            cout<<"NFAtoDFA loop >> grp num :"<<fillingStk.back()->number<<endl;
+            cout<<"\n\nNFAtoDFA loop >> grp num :"<<fillingStk.back()->number<<"with curr states :"<<endl;
             tempStates = fillingStk.back()->states;
-            for(char i =0;i<128;i++){                       // for each symbol in ascii table
-                vector<Node*>* closure = findClosure(findToStates(tempStates,i));
+            StateGroup * currGrp = fillingStk.back();
+            fillingStk.pop_back();
+
+            for(Node *n : *tempStates){
+                cout<<n->nodeNumber;
+            }
+            cout<<endl;
+            for(int i =0;i<128;i++){                       // for each symbol in ascii table
+                vector<Node*>* closure = findClosure(findToStates(tempStates,(char)i));
 
                 /*case 1 : no state in the group go to another under i/p symbol i*/
 
                 if((*closure).size()==0){                   // the epsilon closure is empty
                     cout<<"at i/p :"<<i<<" going to fai"<<endl;
-                    Transition * t = new Transition();
-                    t->toNode = fai;
-                    t->transitionSymbol = i;
-                    fillingStk.back()->getGroupNode()->transitions.push_back(*t);
+                    Transition * t = new Transition(faiG->getGroupNode(),i);
+                    currGrp->getGroupNode()->transitions.push_back(t);
                     continue;
                 }
                 ctr++;
@@ -139,11 +146,10 @@ public :
                         cout<<"at i/p :"<<i<<" going to existing group num :"<<sg->number<<endl;
                         exists=true;
                         ctr--;
-                        Transition * t = new Transition();
-                        t->toNode = sg->getGroupNode();
-                        t->transitionSymbol = i;
-                        fillingStk.back()->getGroupNode()->transitions.push_back(*t);
-                        //put the damn info to the current node in fillingst.ba
+                        Transition * t = new Transition(sg->getGroupNode(),i);
+
+                        currGrp->getGroupNode()->transitions.push_back(t);
+
                         break;
                     }
                 }
@@ -152,19 +158,16 @@ public :
                 /*case 3 : creating new table group*/
 
                 if(!exists){                                  // if it doesn't exist we push it else it will be neglected
-                    Transition * t = new Transition();
+                    Transition * t = new Transition(st->getGroupNode(),i);
                     cout<<"at i/p :"<<i<<" going to new created grp group num :"<<st->number<<endl;
-                    t->toNode = st->getGroupNode();
-                    t->transitionSymbol = i;
-                    fillingStk.back()->getGroupNode()->transitions.push_back(*t);
+
+                    currGrp->getGroupNode()->transitions.push_back(t);
                     groupesTable.push_back(st);
                     fillingStk.push_back(st);
                 }
             }
-            fillingStk.pop_back();
 
         }
-
         groupesTable.push_back(faiG);
 
     }
@@ -174,21 +177,15 @@ public :
 
 
     vector<Node *> getResult(){
-        vector<Node*> *res = new vector<Node>();
-        int i=0;
-        while(!groupesTable.empty()){
+        vector<Node*> *res = new vector<Node*>();
+        for(int i=0;i<groupesTable.size();i++){
             res->push_back((groupesTable.at(i)->getGroupNode()));
-            i++;
         }
-        res->push_back(fai);
-
-
-
 
         for(Node *n : *res){
-            cout<<(n.start ? "ST-":"---")<<n->Number<<":::";
-            for(char i=0;i<128;i++){
-                cout<< "( "<<i<<","<<n->transitions.at(i).toNode->Number<<") -";
+            cout<<(n->start ? "ST-":"---")<<(n->final ? "f-  ":"--  ")<< n->nodeNumber<<":::";
+            for(int  i=48;i<50;i++){
+                cout<< "( "<<i<<","<<n->transitions.at(i)->toNode->nodeNumber<<") -";
             }
             cout<<endl;
         }
