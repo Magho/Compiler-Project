b790e0198334abde981ce6ce07169c41b6d45a77
abdelrahman882 <abdelrahmanahmed882@gmail.com>
2019-03-20 08:02:36 +0200

    adding comments with their copies in branch NFAtoDFA

--- Node.h
+++ Node.h
@@ -1,18 +1,49 @@
 #include<vector>
 #include<string>
+#include "Transition.h"
+
+using namespace std;
+
+class Node {
+public:
+    int nodeNumber =0;
+    bool start;
+    bool final;
+    string lexeme = "";
+    vector<Transition*> transitions;
+    int priority;
+    string tokenName = "";
+public:
+    Node (int Number, bool start, bool final, int priority, string tokenName);   //Tested
+    void addTransition (Node* node_to, char transitionSymbol);                   //Tested
+    vector<Transition*> getPossibleTransitions ();// return vector of pointers   //Tested
+    vector<Node*> getNextNode (char transSymbol);                                //Tested
+    bool isStart();
+    bool isFinal();
+    void setStart();
+    void setFinal();
+    void removeStart();
+    void removeFinal();
+    string getLexeme();                                                          //Tested
+    int getPriority();
+    int getNodeNumber();                                                         //Tested
+    void setNodeNumber(int nodenumber);                                          //Tested
+    string getTokenName();
+};
+/*#include<vector>
+#include<string>
 
 using namespace std;
 
 class Transition;
 
 class Node {
-    private:
+    public:
         int Number;
         bool start;
         bool final;
         string tokenName;
         vector<Transition> transitions;
-    public:
         Node (int Number, bool start, bool final, string tokenName);
         void addTransition (Node& node_to, char transitionSymbol);
         vector<Transition> getPossibleTransitions ();
@@ -20,3 +51,4 @@ class Node {
         bool isStart(Node node);
         bool isFinal(Node node);
 };
+*/
--- Transition.h
+++ Transition.h
@@ -1,11 +1,37 @@
 #include<vector>
 #include<string>
-#include "Node.h"
+
+using namespace std;
+
+
+class Node;
+
+class Transition {
+public :
+
+    Node* toNode; // pointer to the node -- we don't want a copy we need the node itself
+    char transitionSymbol;
+
+    Transition(Node* node, char symbol ) {
+        this->toNode = node;
+        this->transitionSymbol = symbol;
+    };
+};
+
+
+
+/*
+
+#include<vector>
+#include<string>
+#include "Node.cpp"
 
 using namespace std;
 
 class Transition {
     public :
-        Node toNode;
+        Node *toNode;
         char transitionSymbol;
 };
+
+*/
