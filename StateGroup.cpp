a61cac1673a0ba7cb53c171e74681155aa2e34d0
abdelrahman882 <abdelrahmanahmed882@gmail.com>
2019-03-20 06:47:32 +0200

    fixing multi include issue

--- StateGroup.cpp
+++ StateGroup.cpp
@@ -1,69 +1,61 @@
-#include <iostream>
-#include <vector>
-#include "Node.h"
+#include "StateGroup.h"
 using namespace std;
 
 
-class StateGroup{
-private:
-        Node *potentialNode;
+vector<Node *>* states;
+int number =-1;
+StateGroup::StateGroup(int Num){
+    states = new vector<Node*>();
+    number = Num;
+    potentialNode = new Node(Num,false,false,INT_MAX,"");
+}
 
-public :
-    vector<Node *>* states;
-    int number =-1;
-    StateGroup(int Num){
-        states = new vector<Node*>();
-        number = Num;
-        potentialNode = new Node(Num,false,false,INT_MAX,"");
+Node* StateGroup::getGroupNode(){
+    int highest_priority = INT_MAX;
+    for(Node *toAdd : *states){
+        potentialNode->start =potentialNode->start|| toAdd->start;
+        potentialNode->final =potentialNode->final||toAdd->final;
+        if(toAdd->priority< highest_priority){
+            highest_priority = toAdd->priority;
+            potentialNode->priority = toAdd->priority;
+            potentialNode->tokenName = toAdd->tokenName;
+        }
+    }
+    return potentialNode;
+}
+bool StateGroup::addState(Node * toAdd){
+    for (Node *s : *states){
+
+        if(s->nodeNumber== toAdd->nodeNumber){
+
+            return false;
+        }
     }
 
-    Node* getGroupNode(){
-        int highest_priority = INT_MAX;
-        for(Node *toAdd : *states){
-            potentialNode->start =potentialNode->start|| toAdd->start;
-            potentialNode->final =potentialNode->final||toAdd->final;
-            if(toAdd->priority< highest_priority){
-                    highest_priority = toAdd->priority;
-                    potentialNode->priority = toAdd->priority;
-                    potentialNode->tokenName = toAdd->tokenName;
+    states->push_back(toAdd);
+
+    return true;
+}
+
+bool StateGroup::operator ==(StateGroup s2){
+    int num = s2.states->size();
+    for(Node *sElem : *states){
+        int prevNum =num;
+        for(Node *s2Elem : *s2.states){
+            if(sElem->nodeNumber==s2Elem->nodeNumber){
+                num--;
+                break;
             }
         }
-        return potentialNode;
+        if(prevNum==num) return false;
     }
-    bool addState(Node * toAdd){
-        for (Node *s : *states){
-
-            if(s->nodeNumber== toAdd->nodeNumber){
-
-                return false;
-            }
-        }
-
-        states->push_back(toAdd);
-
+    if(num==0){
         return true;
+    }else if(num > 0){
+        return false;
+    }else{
+        return false;
+        //Error -- a potential repeated element in group
     }
+}
 
-    bool operator ==(StateGroup s2){
-        int num = s2.states->size();
-        for(Node *sElem : *states){
-            int prevNum =num;
-            for(Node *s2Elem : *s2.states){
-                if(sElem->nodeNumber==s2Elem->nodeNumber){
-                    num--;
-                    break;
-                }
-            }
-            if(prevNum==num) return false;
-        }
-        if(num==0){
-            return true;
-        }else if(num > 0){
-            return false;
-        }else{
-            return false;
-            //Error -- a potential repeated element in group
-        }
-    }
-
-};
---
+++ StateGroup.h
@@ -0,0 +1,23 @@
+#ifndef STATEGROUP_H
+#define STATEGROUP_H
+#include <iostream>
+#include <vector>
+#include "Node.h"
+
+using namespace std;
+
+class StateGroup{
+private:
+    Node *potentialNode;
+
+public :
+    vector<Node *>* states;
+    int number =-1;
+    StateGroup(int Num);
+    Node* getGroupNode();
+    bool addState(Node * toAdd);
+    bool operator ==(StateGroup s2);
+
+};
+#endif
+
