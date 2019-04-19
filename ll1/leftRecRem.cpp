a82635a96a053f125e3799a0ca623e7735d99613
abdelrahman882 <abdelrahmanahmed882@gmail.com>
2019-04-19 19:13:36 +0200

    implementing preformRecRem main logic

--- ll1/leftRecRem.cpp
+++ ll1/leftRecRem.cpp
@@ -35,15 +35,37 @@ where Aj  1
  productions
 }
  * */
+void directRemoveLR(int index){
 
+}
 bool  leftRecursionRemover::preformLL1(bool debug){
-    for(int i=0;i<rules.size();i++){
-            for(int k=0;k<rules.at(i)->rhs->size();k++){
-                if(rules.at(i)->rhs->at(k))
-                for(int j=0;j<i;j++){
-
+    int originalSize=rules.size(); // am doing so bcuz i'll add el rules elgdida with pushback op O(1) and i do not want to loop into the new prods FOR NOW
+    for(int i=0;i<originalSize;i++){
+            for(int k=rules.at(i)->rhs->size()-1;k>=0;k--){
+                Production *p = rules.at(i)->rhs->at(k);
+                ProductionElement  *e  =p->getProductionVals()->at(0);
+                if(e->isNonTerminal()) {
+                    for (int j = 0; j < i; j++) {
+                        if(rules.at(j)->lhs == e->getSymbolValue()){
+                            //remove e from prod, distribute the rest of prod to rules.at j as new prods for each , add these prods to rules at i and remove org prod
+                            rules.at(i)->rhs->erase(rules.at(i)->rhs->begin()+k); // remove current prod
+                            p->getProductionVals()->erase( p->getProductionVals()->begin() ); // remove first elem
+                            for(int x =0;x<rules.at(j)->rhs->size();x++){
+                                Production *newP = new Production();
+                                for(int z=0;z<rules.at(j)->rhs->at(x)->getProductionVals()->size();z++) {
+                                    newP->appendNewProductionElement(rules.at(j)->rhs->at(x)->getProductionVals()->at(z));
+                                }
+                                for(int z=0;z<p->getProductionVals()->size();z++) {
+                                    newP->appendNewProductionElement(p->getProductionVals()->at(z));
+                                }
+                                rules.at(i)->rhs->push_back(newP);
+                            }
+                        }
+                    }
                 }
-        }
+            }
+            directRemoveLR(i);
+
     }
 
     return false;
