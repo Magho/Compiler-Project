3190488f73fd77cc911e2a07d4e74f9e8fafc21c
BassamMattar <bassammattar2099@gmail.com>
2019-04-19 10:55:35 +0200

    Adding CFG sekeleton and implementing structure creation methods

---
+++ ProductionElement.h
@@ -0,0 +1,24 @@
+//
+// Created by  on 19/04/2019.
+//
+
+#ifndef COMPILERPROJECT_PRODUCTIONELEMENT_H
+#define COMPILERPROJECT_PRODUCTIONELEMENT_H
+#include <iostream>
+
+using namespace std;
+class ProductionElement {
+private:
+    int DEBUG = 1;
+    int terminal = 0;
+    string symbolValue;
+public:
+    ProductionElement(int isTerminal, string symbolValue);
+    string getSymbolValue();
+    int isTerminal();
+    int isNonTerminal();
+    void debug();
+};
+
+
+#endif //COMPILERPROJECT_PRODUCTIONELEMENT_H
