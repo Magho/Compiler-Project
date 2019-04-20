6375c485d0571539aeba2cd15683a9b95d7d6437
BassamMattar <bassammattar2099@gmail.com>
2019-04-20 12:34:22 +0200

    Implementing Grammar input parser

---
+++ GrammarParser.h
@@ -0,0 +1,26 @@
+//
+// Created by ecc on 19/04/2019.
+//
+
+#ifndef COMPILERPROJECT_GRAMMERPARSER_H
+#define COMPILERPROJECT_GRAMMERPARSER_H
+#include <fstream>
+#include "CFG.h"
+#define EPSILON "\L"
+class GrammarParser {
+private:
+    int DEBUG = 1;
+    int startingDetected = 0;
+    CFG grammer;
+public:
+    GrammarParser(string inputFile);
+    CFG getGrammer();
+
+private:
+    string getStringUntil(string &str, string x);
+    string trim(string &str, string deli);
+    Production* handleProductionString(string p);
+};
+
+
+#endif //COMPILERPROJECT_GRAMMERPARSER_H
