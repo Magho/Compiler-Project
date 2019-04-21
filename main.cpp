67d368f2e998d3a3bcf6aeb93f75e04835d388ff
Sajed Hassan <hassanalmorsy1959@gmail.com>
2019-04-21 22:58:12 +0200

    Add phase2 simulator test to main

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include "GeneratingNFA/Converter.h"
 #include "MinimizedDFA/NFAtoDFA.h"
 #include "MinimizedDFA/Minimization.h"
+#include "FirstAndFollow/FirstAndFollowLogic.h"
 
 using namespace std;
 int main(){
@@ -28,6 +29,29 @@ int main(){
     Simulator* simulator = new Simulator(v);
     simulator->resetInputFile("/home/sajed/CLionProjects/Compilers_Phase1/testInput.txt");
     string nextToken = "";
-    simulator->generateTokensFileAndSymbolTable();
+//    simulator->generateTokensFileAndSymbolTable();
+
+     //PHASE 2
+
+    CFG * cfg = new CFG();
+    GrammarParser gp = * new GrammarParser("/home/sajed/CLionProjects/Compiler_Phase2/grammarInput.txt");
+
+    vector<ProductionElement*> terminals = cfg->terminals;
+    vector<ProductionElement*> nonTerminals = cfg->nonTerminals;
+    ProductionElement* start = cfg->startingProductionElement;
+
+    FirstAndFollowLogic * firstAndFollowLogic = new FirstAndFollowLogic(cfg);
+    map<string, vector<ProductionElement>> firstSet = firstAndFollowLogic->getFirsttSet();
+    map<string, vector<ProductionElement>> followSet = firstAndFollowLogic->getFollowtSet();
+
+//    TransitionTable * transitionTable = new TransitionTable(terminals, nonTerminals,
+//                                                            firstSet, followSet);
+//
+//    PredictiveParser pp = * new PredictiveParser(start, transitionTable);
+//    pp.generateLeftMostDerivation(simulator);
+
+
     return 0;
 }
+
+
